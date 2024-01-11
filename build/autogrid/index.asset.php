9fe9ded2d7bb9cfb37a5afe864695e6c19066173
Andreslav <andreslav.k.v@mail.ru>
2024-01-11 17:20:50 +0300

    Уточнил css

--- build/autogrid/index.asset.php
+++ build/autogrid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b9dde5680d2c057afa6d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f2eb2430760420c8bad2');
